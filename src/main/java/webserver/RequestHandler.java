043cf83b9abd14e33b213001fdbe108c2684d188
tommy8808 <tommyguy8808@gmail.com>
2018-09-15 18:39:34 +0900

    input 헤더에서 url 정보를 찾아 주소를 찾아가는 기능 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,15 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.StringTokenizer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +18,8 @@ public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
+    
+    BufferedReader br = null;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -23,15 +30,36 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            
+            br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            
+            String line = br.readLine();
+            String url = null;
+            url = findUrl(line);
+           /* while((line = br.readLine()) != null){
+            	if(line != null){
+            		System.out.println(line);
+            	}
+            }*/
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
+
+	private String findUrl(String line) {
+		String url = "";
+		StringTokenizer st = new StringTokenizer(line, " ");
+		while(st.hasMoreTokens()){
+			if((url = st.nextToken()).startsWith("/")){
+				break;
+			}
+		}
+		return url;
+	}
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
